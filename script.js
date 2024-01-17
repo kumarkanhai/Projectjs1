3ee669493a029635c5b5e41bad86348704862862
kumarkanhai <kanhaikumarupvns@gmail.com>
2024-01-17 19:31:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('#add').addEventListener('click',f1);
+function f1(){
+    document.querySelector('h5').innerText=  'Friends';
+    document.querySelector('h5').style.color='green';
+}
